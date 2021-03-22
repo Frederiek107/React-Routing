cb741f15bf4b06a252c1db5c2de1d4cdfbe8f164
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-03-22 14:03:54 +0100

    finished assignment

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,31 +1,37 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 function TopMenu() {
-  return (
-    <nav>
-      <div className="nav-container">
-        <h4>De Tandenborstel</h4>
+    return (
+        <nav>
+            <div className="nav-container">
+                <h4>De Tandenborstel</h4>
 
-        <ul>
-          <li>
-            Home
-          </li>
-
-          <li>
-            Gaatjes
-          </li>
-
-          <li>
-            Afspraak maken
-          </li>
-
-          <li>
-            Tanden bleken
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+                <ul>
+                    <NavLink to="/" exact activeClassName="active-link">
+                        <li>
+                            Home
+                        </li>
+                    </NavLink>
+                    <NavLink to="/gaatjes" activeClassName="active-link">
+                        <li>
+                            Gaatjes
+                        </li>
+                    </NavLink>
+                    <NavLink to="/afspraak-maken" activeClassName="active-link">
+                        <li>
+                            Afspraak maken
+                        </li>
+                    </NavLink>
+                    <NavLink to="/tanden-bleken" activeClassName="active-link">
+                        <li>
+                            Tanden bleken
+                        </li>
+                    </NavLink>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default TopMenu;
